476c51ce323f80bbfcadaf285e470d8f8b849346
Dylan Crowley <dcrowdev1025@gmail.com>
2022-10-14 10:21:03 -0400

    retheme/structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// $(document).ready(function () {
+$(document).ready(function () {
 
 
 
@@ -6,41 +6,42 @@
 
 
 
-// function searchGame(e) {
-// e.preventDefault();
-// //get input value
-// //fetch api to browse games by catagory
-// //change location
-    // fetch youtube video
-// //append info/video to page
-// }
-
-
-
-
-
-
-function  fetchTest() {
-fetch('https://api.rawg.io/api/genres?key=5d6305c20a2d4927a017ef5ef6beab60')
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
-
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'fc02a3c87bmshe9e61389afea8d6p11e23ejsn3996352c92c2',
-		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
-	}
-};
-
-fetch('https://youtube138.p.rapidapi.com/video/streaming-data/?id=VyHV0BRtdxo', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+function searchGame(e) {
+e.preventDefault();
+console.log('hi')
+//get input value
+//fetch api to browse games by catagory
+//change location
+    window.location.href = "./stats.html/"
+//append info/video to page
 }
-fetchTest();
+
+
+
+
+
+
+// function  fetchTest() {
+// fetch('https://api.rawg.io/api/genres?key=5d6305c20a2d4927a017ef5ef6beab60')
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'fc02a3c87bmshe9e61389afea8d6p11e23ejsn3996352c92c2',
+// 		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://youtube138.p.rapidapi.com/video/streaming-data/?id=VyHV0BRtdxo', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+// }
+// fetchTest();
 
 
 
@@ -51,10 +52,10 @@ fetchTest();
 
 
 //event listeners
-$('###').on('submit', searchGame);
+$('#searchBtn').on('click', searchGame);
 
 
 
 
 
-// })
+});
