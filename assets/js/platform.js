7d5d76c90cafd1cdfd0f89991e592c9be2c5b32d
Dylan Crowley <dcrowdev1025@gmail.com>
2022-10-17 13:38:14 -0400

    added height for no return

--- assets/js/platform.js
+++ assets/js/platform.js
@@ -9,7 +9,7 @@ fetch('https://api.rawg.io/api/platforms/' + platform + '?key=5d6305c20a2d4927a0
     .then(data => { console.log(data)
         $('#platform-image').attr('src', data.image_background);
         $('#platform-title').text(platformName);
-        
+        $('#total-games').text(data.games_count);
 
 })
 
@@ -17,7 +17,21 @@ fetch('https://api.rawg.io/api/platforms/' + platform + '?key=5d6305c20a2d4927a0
 
 
 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'fc02a3c87bmshe9e61389afea8d6p11e23ejsn3996352c92c2',
+		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+	}
+};
 
+fetch('https://youtube138.p.rapidapi.com/search/?q=' + platformName + 'gaming news' + '&hl=en&gl=US', options)
+	.then(response => response.json())
+	.then(data => { console.log(data)
+        const video = 'https://www.youtube.com/embed/' + data.contents[0].video.videoId;
+        $('.video-container').append('<iframe class="video-frame" src="' + video + '"></iframe>')
+})
+	.catch(err => console.error(err));
 
 
 
