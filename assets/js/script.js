08e303fd64f5abc49c6b56fe3d9a421dc549db8b
Dylan Crowley <dcrowdev1025@gmail.com>
2022-10-17 10:40:40 -0400

    made top game links clickable

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,27 +14,27 @@ fetch('https://api.rawg.io/api/platforms?key=5d6305c20a2d4927a017ef5ef6beab60')
             $('.dropdown-item').append('<a class="dropdown-anchor" id="' + data.results[i].id + '">' + data.results[i].name + '</a>');
         if (data.results[i].name == 'PC') {
             for (let j = 0; j < data.results[i].games.length; j++) {
-            $('#pc-list').append('<li class="pc-list-item">' + data.results[i].games[j].name + '</li>')
+            $('#pc-list').append('<li class="pc-list-item"><a class="clickable-game">' + data.results[i].games[j].name + '</a></li>')
             }
         }
          if (data.results[i].name == 'PlayStation 5') {
             for (let j = 0; j < data.results[i].games.length; j++) {
-            $('#ps5-list').append('<li class="playstation-list-item">' + data.results[i].games[j].name + '</li>')
+            $('#ps5-list').append('<li class="playstation-list-item"><a class="clickable-game">' + data.results[i].games[j].name + '</a></li>')
             }
         }
          if (data.results[i].name == 'Xbox One') {
             for (let j = 0; j < data.results[i].games.length; j++) {
-            $('#xbox-list').append('<li class="xbox-list-item">' + data.results[i].games[j].name + '</li>')
+            $('#xbox-list').append('<li class="xbox-list-item"><a class="clickable-game">' + data.results[i].games[j].name + '</a></li>')
             }
         }
          if (data.results[i].name == 'iOS') {
             for (let j = 0; j < data.results[i].games.length; j++) {
-            $('#ios-list').append('<li class="ios-list-item">' + data.results[i].games[j].name + '</li>')
+            $('#ios-list').append('<li class="ios-list-item"><a class="clickable-game">' + data.results[i].games[j].name + '</a></li>')
             }
         }
          if (data.results[i].name == 'Android') {
             for (let j = 0; j < data.results[i].games.length; j++) {
-            $('#android-list').append('<li class="android-list-item">' + data.results[i].games[j].name + '</li>')
+            $('#android-list').append('<li class="android-list-item"><a class="clickable-game">' + data.results[i].games[j].name + '</a></li>')
             }
         }
         }
@@ -49,7 +49,7 @@ if(!$('#game-search').val()) {
 alert('Please enter a game')
 return;
 }
-const game = $('#game-search').val().replaceAll(' ', '-').trim();
+const game = $('#game-search').val().replaceAll(' ', '-').replaceAll(':', '').toLowerCase().trim();
 localStorage.setItem("game", game);
 window.location.href = "game.html"
 }
@@ -64,7 +64,13 @@ window.location.href = "platform.html"
 }
 
 function searchClickedGame () {
-const game = $(this).attr('id').trim();
+const game = $(this).attr('id').toLowerCase();
+localStorage.setItem("game", game);
+window.location.href = "game.html"
+}
+
+function anotherSearchedClickedGame () {
+const game = $(this).text().replaceAll(' ', '-').replaceAll(':', '').toLowerCase().trim();
 localStorage.setItem("game", game);
 window.location.href = "game.html"
 }
@@ -82,6 +88,7 @@ window.location.href = "game.html"
 $('#searchBtn').on('click', searchGame);
 $(document).on('click', '.dropdown-anchor', searchPlatform);
 $('.carousel-games').on('click', searchClickedGame)
+$(document).on('click', '.clickable-game', anotherSearchedClickedGame)
 new Splide( '.splide', {
   type    : 'loop',
   autoplay: 'play',
